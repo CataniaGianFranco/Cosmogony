6b25279d50fc13caea31048f15fc559b9635c6d3
Gian <catania.gian@gmail.com>
2022-10-12 01:09:52 -0300

    Se crearon enemigos y aliados.

--- scripts/enemy_buffalo.gd
+++ scripts/enemy_buffalo.gd
@@ -1,6 +1,9 @@
 extends Node2D
 var _health: int = 20 
-onready var _animated_sprite: AnimatedSprite = $AnimatedSprite 
+onready var _animation_player: AnimationPlayer = $AnimationPlayer 
+
+func _process(delta: float) -> void:
+	_animation_player.play("idle")
 
 func take_damage(amount : int) -> void:
 	#_animated_sprite.Play("Hurt")
