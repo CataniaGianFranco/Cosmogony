2fd52a6b00ba2feee9e0a584e74d29511743d8fb
Gian <catania.gian@gmail.com>
2022-10-18 00:26:10 -0300

    Piedra de runa ya programada.

---
+++ assets/collects/runes/StoneRune.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+export(String) var name_animation: String = ""
+export(SpriteFrames) var frames_rune: SpriteFrames = null
+
+var stone_rune_on: bool = false
+
+func _ready() -> void:
+	$AnimatedSprite.frames = frames_rune
+	$AnimatedSprite.animation = name_animation
+
+func _on_StoneRune_body_entered(body: Node) -> void:
+	if body.is_in_group("Player") and stone_rune_on == false:
+		$Sprite.visible = false
+		$AnimatedSprite.play()
+		$AudioStreamPlayer2D.play()
+		stone_rune_on = true
