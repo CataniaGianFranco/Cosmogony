ec43a0e8f860d4eb91b561dc44fdcb09e7cf839a
Gian <catania.gian@gmail.com>
2022-10-28 15:21:15 -0300

    Fixs Buffalo

--- assets/characters/transforms/buffalo/buffalo.gd
+++ assets/characters/transforms/buffalo/buffalo.gd
@@ -1,16 +1,13 @@
-extends Node2D
+extends Position2D
 
 var _enemy_buffalo_position: Vector2 = Vector2(0,0)
 var _buffalo_position: Vector2 = Vector2(0,0)
 
 var _can_change_creature: bool = true 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	if $EnemyBuffalo != null:
-		_enemy_buffalo_position = $EnemyBuffalo.position
-	elif $EnemyBuffalo == null and _can_change_creature == true:
-		$AllyBuffalo.position = _enemy_buffalo_position
-		$AllyBuffalo.visible = true
-		_can_change_creature = false
+func _ready() -> void:
+	pass
+
+func _on_EnemyBuffalo_tree_exited() -> void:
+	$AllyBuffalo.visible = true
+	_can_change_creature = false
