5d0faf81f3e25fba180db8652f8b60ef4f0a05d7
Gian <catania.gian@gmail.com>
2022-11-02 01:04:29 -0300

    Se programo enemigo búffalo la IA

--- assets/characters/npcs/enemies/enemy_buffalo/scripts/enemy_buffalo.gd
+++ assets/characters/npcs/enemies/enemy_buffalo/scripts/enemy_buffalo.gd
@@ -1,17 +1,44 @@
 extends KinematicBody2D
 
-var _health: int = 50 
+var _health: int = 50
+
+const MAX_SPEED : float = 100.0
+const GRAVITY : float = 25.0
+
+var _motion : Vector2 = Vector2.ZERO
 
 onready var _animation_player: AnimationPlayer = $AnimationPlayer 
+onready var _sprite : Sprite = $Sprite
 
 func _ready() -> void:
-	_animation_player.play("idle")
+	_sprite.scale.x = -1
+	_animation_player.play("walk")
+	_motion.x = MAX_SPEED
+
+func _process(delta: float) -> void:
+	_motion.y += GRAVITY
+	flip()
+	
+	_motion = move_and_slide(_motion)
+
+func next_to_left_wall() -> bool:
+	return $LeftRay.is_colliding()
+func next_to_right_wall() -> bool:
+	return $RightRay.is_colliding()
+func floor_detection() -> bool:
+	return $Sprite/FloorDetection.is_colliding()
+
+func flip() -> void:
+	if next_to_right_wall() or next_to_left_wall() or !floor_detection():
+		_motion.x *= -1
+		_sprite.scale.x *= -1 
 
 func take_damage(amount : int) -> void:
 		_health -= amount
 		if _health > 0:
 			_animation_player.play("hurt")
 		else:
+			_motion.x = 0.0
 			_animation_player.play("exhausted")
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
