e2874e5a3219a4f96a86132510d582c558ed6f82
Gian <catania.gian@gmail.com>
2022-11-18 00:09:23 -0300

    Maiin menu

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -1,12 +1,15 @@
 extends VBoxContainer
 
+func _ready() -> void:
+	full_screen()
+
 func _process(_delta: float) -> void:
 	full_screen()
 
 func full_screen() -> void:
-	if $VideoSettings/CheckBox.pressed == true:
-		OS.window_fullscreen = true
-		$VideoSettings/CheckBox.text = "On"
-	else:
+	if $VideoSettings/CheckBox.pressed == false:
 		OS.window_fullscreen = false
 		$VideoSettings/CheckBox.text = "Off"
+	else:
+		OS.window_fullscreen = true
+		$VideoSettings/CheckBox.text = "On"
